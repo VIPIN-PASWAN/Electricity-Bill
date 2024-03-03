e5e04db31a8b1674fae5bf925d3215f85a3d50ae
VIPIN-PASWAN <vipin1911016@akgec.ac.in>
2024-03-03 14:37:28 +0530

    Added paybill html, css & js file

---
+++ paybill.js
@@ -0,0 +1,9 @@
+document.getElementById("logoutbtn").addEventListener("click", function(){ 
+    alert("You have been successfully logedout.")
+});
+
+document.getElementById("paybtn").addEventListener("click", function(){
+    alert("page will be open sortly.");
+});
+
+// document.getElementById("payable").innerHTML
